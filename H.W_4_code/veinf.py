7707c3628c00cc3e502b308dde8b43001904d7cc
Kuntal Pal <kpal002@ucr.edu>
2022-02-05 00:25:22 -0800

    Add files via upload

--- H.W_4_code/veinf.py
+++ H.W_4_code/veinf.py
@@ -1,3 +1,4 @@
+from functools import reduce
 # implements VE inference
 # Note: you should *not* change the passed in factorset, fs
 # Make your own copy to work on if you need to.
@@ -17,18 +18,72 @@
 #  order specified by elimorder (ignoring variables that should stay)
 #  and, after all of those are done, eliminate variables not mentioned
 #  in elimorder
+
+
+def ve(factors,elimorder):
+    
+    phi = factors
+    varriables = elimorder
+    phi_dprime = set(phi)
+
+
+    
+    if elimorder == []:
+        return set()
+    
+    # Loop over variables to be eliminated
+    for var in elimorder:
+        phi_prime = set()
+        
+        #print(var)
+        for fac in phi:
+            scope = fac.vars
+
+            if var in scope:
+                
+                phi_prime = phi_prime.union({fac})
+                
+                phi_dprime = phi_dprime - phi_prime
+    
+                    
+        psi = reduce((lambda x, y: x * y), phi_prime)
+
+        tau = psi.marginalize({var})
+        #print(tau)
+        phi_dprime = phi_dprime.union({tau})
+        phi = phi_dprime
+
+    
+    return phi_dprime.union({tau})
+
 def veinf(fs,X,Y,elimorder=[]):
 
     # TO BE IMPLEMENTED BY YOU!
 
-# same as above, but y is an assignment (not just a set of variables)
-# and so the returned factor is just over X (the probabilty of X given Y=y)
-# see veinf about elimorder!!
-# reduce the factors before eliminating (otherwise, you will do too much
-#  computational work)
-def veinfval(fs,X,y,elimorder=[]):
-    # in case this helps:
-    # y is a dictionary (mapping from variables to values)
-    # thus y.keys() is the set of variables
+    # same as above, but y is an assignment (not just a set of variables)
+    # and so the returned factor is just over X (the probabilty of X given Y=y)
+    # see veinf about elimorder!!
+    # reduce the factors before eliminating (otherwise, you will do too much
+    #  computational work)
+    elimorder = sorted(set(elimorder), key=elimorder.index)
 
-    # TO BE IMPLEMENTED BY YOU!
+    ini_eli = [item for item in elimorder if item not in list(X.union(Y))]
+    final_eli = [item for item in list(fs.vars) if ((item not in ini_eli) and (item not in list(X.union(Y))))]
+    
+    phi = ve(fs.factors,ini_eli)
+    if len(ini_eli) == 0:
+        phi_star = ve(fs.factors,final_eli)
+    elif len(final_eli) == 0:
+        phi_star = phi
+    else:
+        phi_star = ve(list(phi),final_eli)
+
+    phi_star_new = reduce((lambda x, y: x * y), phi_star)
+    phi_star_new = phi_star_new/phi_star_new.marginalize(X)
+    return phi_star_new
+
+def veinfval(fs,X,y,elimorder=[]):
+
+    return veinf(fs,X,y.keys(),elimorder=[]).reduce(y)
+
+
